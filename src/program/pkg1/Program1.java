7710899012cd892e19d2eccc03b278db24133095
PirateKingDee <andyliang987@hotmail.com>
2017-04-05 21:22:16 -0700

    Project 1 work

--- src/program/pkg1/Program1.java
+++ src/program/pkg1/Program1.java
@@ -5,6 +5,8 @@
  */
 package program.pkg1;
 
+import java.io.File;
+import java.util.Scanner;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import static org.lwjgl.opengl.GL11.*;
@@ -30,7 +32,7 @@ public class Program1{
     }
     
     private void initGL(){
-        glClearColor(1f, 1f, 1f, 0.1f);
+        glClearColor(0f, 0f, 0f, 0.0f);
         
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
@@ -46,50 +48,42 @@ public class Program1{
             try{
                 glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
                 glLoadIdentity();
-                glColor3f(0.7f, 0.3f, 0.3f);
-                //glPointSize(10);
                 
-//                glBegin(GL_LINES);
-//                    glVertex2f(10.0f, 380.0f);
-//                    glVertex2f(380.0f, 10.0f);
-//                glEnd();
-//                glBegin(GL_POINTS);
-//                    glVertex2f(59.1f, 299f);
-//                glEnd();
-                float x1, y1, x2, y2;
-                //case 1
-//                x1 = 10;
-//                y1 = 100;
-//                x2 = 100;
-//                y2 = 120;
-                //case 2
-//                x2 = 10;
-//                y2 = 100;
-//                x1 = 100;
-//                y1 = 130;
-                //case 3, need to work on
-                x2 = 10;
-                y2 = 130;
-                x1 = 100;
-                y1 = 100;
-//                //case 4
-//                x1 = 10;
-//                y1 = 130;
-//                x2 = 100;
-//                y2 = 100;
-                drawLine(x1,y1,x2,y2);
-//                drawLine(350, 50, 500, 220);
-                
-                glColor3f(0.5f, 0.3f, 0.9f);
-                glPointSize(3);
-                glBegin(GL_POINTS);
-                    glVertex2f(x1, y1);
-                    glVertex2f(x2, y2);
-                    //glVertex2f(500, 220);
-                glEnd();
-                
-                Display.update();
-                Display.sync(60);
+                try{
+                    File file = new File("src/coordinates.txt");
+                    Scanner fileScanner = new Scanner(file);
+                    while(fileScanner.hasNextLine()){
+                        String str = fileScanner.nextLine();
+                        String[] splitString = str.split(" ");
+                        char figure = splitString[0].charAt(0);
+                        switch(figure){
+                            case 'l': String[] point1 = splitString[1].split(",");
+                                      String[] point2 = splitString[2].split(",");
+                                      drawLine(Float.parseFloat(point1[0]),Float.parseFloat(point1[1]),Float.parseFloat(point2[0]),Float.parseFloat(point2[1]));
+                                      break;
+                            case 'c': String[] circleCenter = splitString[1].split(",");
+                                      drawCircle(Float.parseFloat(circleCenter[0]),Float.parseFloat(circleCenter[1]), Float.parseFloat(splitString[2]));
+                                      break;
+                            case 'e': String[] elipseCenter = splitString[1].split(",");
+                                      String[] elipseRadius = splitString[2].split(",");
+                                      drawElipse(Float.parseFloat(elipseCenter[0]), Float.parseFloat(elipseCenter[1]),
+                                              Float.parseFloat(elipseRadius[0]), Float.parseFloat(elipseRadius[1]));
+                                      break;
+                            default : break;          
+                        }
+                        
+                    }
+//                    drawElipse(100,100, 45,80);
+//                    drawLine(10,380,380,10);
+//                    drawLine(350,50,500,70);
+//                    drawElipse(450,250,75,35);
+//                    drawCircle(50,50,100);
+                    Display.update();
+                    Display.sync(60);
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
             }
             catch(Exception e){
                 
@@ -98,9 +92,24 @@ public class Program1{
         Display.destroy();
     }
     public void drawCircle(float centerX, float centerY, float radius){
-        
+        glColor3f(0f, 0f, 1f);
+        float x, y, theta;
+        float degree = 0;
+        while(degree <= 360){
+            theta = (float) ((degree * Math.PI)/180);
+            x = (float) Math.cos(theta)*radius + centerX;
+            y = (float) Math.sin(theta)*radius + centerY;
+            glBegin(GL_POINTS);
+                glVertex2f(x,y);
+            glEnd();
+            degree++;
+        }   
     }
+    
+    //This function takes the coordinates of two end points of a line and draw the line using 
+    //the midpoint algorithm.
     public void drawLine(float x1, float y1, float x2, float y2){
+        glColor3f(1f, 0f, 0f);
         //Declare and initialize dx, dy, d, incrementRight, incrementUpRight, x, and y
         float dx, dy, d, incrementRight, incrementUpRight, x, y, slope;
         
@@ -143,9 +152,7 @@ public class Program1{
                             glBegin(GL_POINTS);
                                 glVertex2f(x,y);
                             glEnd();
-                            //d = 2(dy-dx)+d(old)
-//                            d = 2*(dy-dx) + d;
-                              d = d + dy - dx;
+                              d = d + incrementUpRight;
                         }
                         else{
                             //x+1
@@ -154,9 +161,7 @@ public class Program1{
                             glBegin(GL_POINTS);
                                 glVertex2f(x,y);
                             glEnd();
-                            //d = d(old)+2dy
-//                            d = d + 2 * dy;
-                              d = d + dy;
+                              d = d + incrementRight;
                         }
                     }
                 }
@@ -261,9 +266,9 @@ public class Program1{
             //d = (0.5 * dy)-dx
             d = (float) ((0.5 * dy) -dx);
             //incrementRight = 2*dy
-            incrementRight = 2 * dy;
+            incrementRight = 2 * dx;
             //incrementUpRight = 2*(dy-dx)
-            incrementUpRight = 2 * (dy - dx);
+            incrementUpRight = 2 * (dx - dy);
             //x = x1
             x = x1;
             //y = y1
@@ -273,39 +278,146 @@ public class Program1{
                 glVertex2f(x,y);
             glEnd();
             //while(x < x2)
-
-            while(y < y2){
-                if(d > 0){
-                    //x+1, y+1
-                    x = x+1;
-                    y = y+1;
-                    //draw new (x,y)
-                    glBegin(GL_POINTS);
-                        glVertex2f(x,y);
-                    glEnd();
-                    //d = 2(dy-dx)+d(old)
-                    d = 2*(dx-dy) + d;
-//                    d = d + dx - dy;
+            if(slope > 0){
+                if(dy > 0 && dy > 0){
+                    //d = (0.5 * dy)-dx
+                    d = (float) ((0.5 * dy) -dx);
+                    while(y < y2){
+                        if(d > 0){
+                            //x+1, y+1
+                            x = x+1;
+                            y = y+1;
+                            //draw new (x,y)
+                            glBegin(GL_POINTS);
+                                glVertex2f(x,y);
+                            glEnd();
+                            //d = 2(dy-dx)+d(old)
+                            d = 2*(dx-dy) + d;
+        //                    d = d + dx - dy;
+                        }
+                        else{
+                            //x+1
+                            y = y + 1;
+                            //draw new (x,y)
+                            glBegin(GL_POINTS);
+                                glVertex2f(x,y);
+                            glEnd();
+                            //d = d(old)+2dy
+                            d = d + 2 * dx;
+        //                    d = d + dx;
+                        }
+                    } 
                 }
-                else{
-                    //x+1
-                    y = y + 1;
-                    //draw new (x,y)
-                    glBegin(GL_POINTS);
-                        glVertex2f(x,y);
-                    glEnd();
-                    //d = d(old)+2dy
-                    d = d + 2 * dx;
-//                    d = d + dx;
+                else if (dy < 0 && dx < 0){
+                    dy = -dy;
+                    dx = -dx;
+                    while(y > y2){
+                        if(d > 0){
+                            //x+1, y+1
+                            x = x-1;
+                            y = y-1;
+                            //draw new (x,y)
+                            glBegin(GL_POINTS);
+                                glVertex2f(x,y);
+                            glEnd();
+                            //d = 2(dy-dx)+d(old)
+                            d = 2*(dx-dy) + d;
+        //                    d = d + dx - dy;
+                        }
+                        else{
+                            //x+1
+                            y = y - 1;
+                            //draw new (x,y)
+                            glBegin(GL_POINTS);
+                                glVertex2f(x,y);
+                            glEnd();
+                            //d = d(old)+2dy
+                            d = d + 2 * dx;
+        //                    d = d + dx;
+                        }
+                    } 
+
                 }
             }
+            else if (slope < 0){
+                if(dy > 0 && dx < 0){
+                    dx = -dx;
+                    while(y < y2){
+                        if(d > 0){
+                            //x+1, y+1
+                            x = x - 1;
+                            y = y + 1;
+                            //draw new (x,y)
+                            glBegin(GL_POINTS);
+                                glVertex2f(x,y);
+                            glEnd();
+                            //d = 2(dy-dx)+d(old)
+                            d = 2*(dx-dy) + d;
+        //                    d = d + dx - dy;
+                        }
+                        else{
+                            //x+1
+                            y = y + 1;
+                            //draw new (x,y)
+                            glBegin(GL_POINTS);
+                                glVertex2f(x,y);
+                            glEnd();
+                            //d = d(old)+2dy
+                            d = d + 2 * dx;
+        //                    d = d + dx;
+                        }
+                    } 
+
+                }
+                else if (dx > 0 && dy < 0){
+                    dy = -dy;
+                    while(y > y2){
+                        if(d > 0){
+                            //x+1, y+1
+                            x = x+1;
+                            y = y-1;
+                            //draw new (x,y)
+                            glBegin(GL_POINTS);
+                                glVertex2f(x,y);
+                            glEnd();
+                            //d = 2(dy-dx)+d(old)
+                            d = 2*(dx-dy) + d;
+        //                    d = d + dx - dy;
+                        }
+                        else{
+                            //x+1
+                            y = y - 1;
+                            //draw new (x,y)
+                            glBegin(GL_POINTS);
+                                glVertex2f(x,y);
+                            glEnd();
+                            //d = d(old)+2dy
+                            d = d + 2 * dx;
+        //                    d = d + dx;
+                        }
+                    } 
+
+                }
+            }
+            
         }
             
                 
                 
     }
-    public void drwaElipse(float centerX, float centerY, float radius1, float radius2){
-        
+    public void drawElipse(float centerX, float centerY, float radius1, float radius2){
+        glColor3f(0f, 1f, 0f);
+        float x, y, theta;
+        float degree = 0;
+        while(degree <= 360){
+            theta = (float) ((degree * Math.PI)/180);
+            x = (float) Math.cos(theta)*radius1 + centerX;
+            y = (float) Math.sin(theta)*radius2 + centerY;
+            glBegin(GL_POINTS);
+                glVertex2f(x,y);
+            glEnd();
+            degree++;
+        }
     }
     
     public static void main(String[] args){
